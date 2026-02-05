3281b5b9e91174c7a9fef3d6155d0446cd738f08
managikeda <manag.ikeda@gmail.com>
2026-02-05 14:31:47 +0900

    出力文末を更新

--- src/App.java
+++ src/App.java
@@ -2,8 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         int a = 20;
         int b = 4;
-        System.out.println(add(a,b));
-        System.out.println(sub(a,b));
+        System.out.println(add(a,b)+"だぞ");
+        System.out.println(sub(a,b)+"だがや");
         System.out.println(multi(a,b));
         System.out.println(div(a,b));
     }
