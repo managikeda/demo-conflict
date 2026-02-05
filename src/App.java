3fa2f6af0c04c39e163bdaa155a2d7cb626f1bde
managikeda <manag.ikeda@gmail.com>
2026-02-05 10:41:15 +0900

    hinagara

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int a = 20;
+        int b = 4;
+        System.out.println(add(a,b));
+        System.out.println(sub(a,b));
+        System.out.println(multi(a,b));
+        System.out.println(div(a,b));
     }
+
+
+    public static int add(int a,int b){
+        return 0;
+    }
+
+
+    public static int sub(int a,int b){
+        return 0;
+    }
+
+
+    public static int multi(int a,int b){
+        return 0;
+    }
+
+
+    public static int div(int a,int b){
+        return 0;
+    }
+
 }
